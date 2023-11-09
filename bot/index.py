ff016c81949870e0a7bd86335faded2836c00290
Davi Ramos <davi.libresolucoes@gmail.com>
2023-11-09 07:52:21 -0300

    Adicionando rabbitmq

--- bot/index.py
+++ bot/index.py
@@ -1,70 +1,76 @@
 from flask import Flask, render_template, request, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
 import qrcode
 import requests
 import time
+import threading
+import pika
+import json
 
 app = Flask(__name__)
+scheduler = BackgroundScheduler()
 
-@app.route('/')
-def index():
-
-    # Function to make a GET request and fetch the QR code content
-    def fetch_qr_content():
-        url = "http://localhost:8000/status"
-        while True:
-            response = requests.get(url)
+# Function to make a GET request and fetch the QR code content
+def fetch_qr_content():
+    fetch_status = "http://localhost:8000/status"
+    fetch_qr_code = "http://localhost:8000/qr-code"
+    
+    response = requests.get(fetch_status)
+    if response.status_code == 200:
+        status_data = response.json()
+        if status_data.get("status") == 1:
+            print('Status 1 recebido, buscando QR Code...')
+            response = requests.get(fetch_qr_code)
             if response.status_code == 200:
                 data = response.json()
                 if data.get("status") == "successo":
-                    return data.get("url")
-                elif data.get("status") == 0:
-                    continue
-                elif data.get("status") == 1:
-                    url = "http://localhost:8000/qr-code"
-                elif data.get("status") == 3:
-                    break
-            elif response.status_code == 404:
-                # If 404, wait for a second and then retry
-                time.sleep(1)
-        return None
+                    qr_code_url = data.get("url")
+                    print('QR Code obtido com sucesso')
+                    gera_qr_code(qr_code_url)
 
-    # Fetch QR code content
-    qr_content = fetch_qr_content()
+def gera_qr_code(qr_code_url):
+    try:
+        # Gera o QR code
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(qr_code_url)
+        qr.make(fit=True)
 
-    # Generating and saving QR code as PNG if content is fetched
-    if qr_content:
-        try:
-            # Generate QR code
-            qr = qrcode.QRCode(
-                version=1,
-                error_correction=qrcode.constants.ERROR_CORRECT_L,
-                box_size=10,
-                border=4,
-            )
-            qr.add_data(qr_content)
-            qr.make(fit=True)
+        # Cria a imagem PNG do QR code
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save("./static/qr_code.png")
+    except Exception as e:
+        print(f"Erro ao gerar o QR Code: {e}")
 
-            # Create a PNG file
-            img = qr.make_image(fill_color="black", back_color="white")
-            img.save("./static/qr_code.png")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-            return render_template('index.html')
-        except Exception as e:
-            return render_template('error.html')
-    else:
-        result = "Failed to fetch QR code content."
+def rabbitmq_consumer():
+    def callback(ch, method, properties, body):
+        message = json.loads(body)
+        print("Mensagem recebida do RabbitMQ: ", message)
+        # Aqui você pode adicionar a lógica para processar a mensagem e responder
 
-@app.route('/conectado', methods=['POST'])
-def conectado():
-    # Logic for connected
-    data = request.json  # This will be the JSON data sent with the POST request
-    return jsonify({"status": "connected", "data": data}), 200
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
 
-@app.route('/mensagem', methods=['POST'])
-def mensagem():
-    # Logic for message
-    message = request.json  # This will be the JSON data sent with the POST request
-    return jsonify({"status": "message received", "message": message}), 200
+    channel.queue_declare(queue='messages', durable=True)
+
+    channel.basic_consume(queue='messages', on_message_callback=callback, auto_ack=True)
+
+    print('Consumidor RabbitMQ iniciado. Esperando por mensagens...')
+    channel.start_consuming()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Iniciando o consumidor RabbitMQ em uma thread separada
+    threading.Thread(target=rabbitmq_consumer).start()
+
+    # Adiciona o job que verifica o status e gera o QR code
+    scheduler.add_job(fetch_qr_content, 'interval', seconds=10)
+    scheduler.start()
+    app.run(debug=True, use_reloader=False)  # use_reloader=False para evitar duplicação do job com o reloader do Flask
